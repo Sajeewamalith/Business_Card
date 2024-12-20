31f847e19fe3432def18688de61a961c5747e482
Sajeewa Malith <sajeewamalith0219@gmail.com>
2024-12-20 19:01:05 +0530

    import optimize & simple fixes

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -10,23 +10,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-
-
-
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -85,7 +78,7 @@ fun BusinessCardScreen() {
     }
     Box(
         modifier = Modifier
-            .padding(bottom = 80.dp),
+            .padding(bottom = 72.dp),
         contentAlignment = Alignment.BottomCenter
     ) {
         Column(
@@ -121,7 +114,9 @@ fun BusinessCardScreen() {
                     fontSize = 20.sp
                 )
             }
-            Row() {
+            Row(
+                modifier = Modifier.padding(bottom = 8.dp)
+            ) {
                 Image(
                     painter = painterResource(id = R.drawable.baseline_email_24),
                     contentDescription = null,
